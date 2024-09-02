89f3f11eee5a086f8b82bb29f0028d5c19a4f839
Dianna Sharmazanyan <dianna.sharmazanyan@gmail.com>
2024-09-02 16:32:58 +0300

    refactor: fix Hexlet check

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -12,26 +12,26 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8')
 describe('genDiff tests', () => {
   const testCases = [
     {
-      description: 'flat json',
+      title: 'flat json',
       file1: 'file1.json',
       file2: 'file2.json',
       expected: 'resultStylish.txt',
     },
     {
-      description: 'flat yaml',
+      title: 'flat yaml',
       file1: 'filepath1.yml',
       file2: 'filepath2.yml',
       expected: 'resultStylish.txt',
     },
     {
-      description: '--format plain',
+      title: '--format plain',
       file1: 'filepath1.yml',
       file2: 'filepath2.yml',
       format: 'plain',
       expected: 'resultPlain.txt',
     },
     {
-      description: '--format json',
+      title: '--format json',
       file1: 'file1.json',
       file2: 'file2.json',
       format: 'json',
@@ -40,9 +40,9 @@ describe('genDiff tests', () => {
   ];
 
   testCases.forEach(({
-    description, file1, file2, format = 'stylish', expected,
+    title, file1, file2, format = 'stylish', expected,
   }) => {
-    test(description, () => {
+    test(title, () => {
       const result = genDiff(getFixturePath(file1), getFixturePath(file2), format);
       expect(result.replaceAll(/\s$/gm, '')).toEqual(readFile(expected).replaceAll(/\s$/gm, ''));
     });
